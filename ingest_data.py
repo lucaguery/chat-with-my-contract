982c23c9aed343961b4446450f9fc92b8c220f36
luca-g-fox <luca.guery@foxintelligence.fr>
2023-12-02 18:03:10 +0100

    cleaning

--- ingest_data.py
+++ ingest_data.py
@@ -1,19 +1,10 @@
-# import pickle
-
-from langchain.document_loaders import TextLoader, PyPDFLoader
+from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings import OpenAIEmbeddings
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain.vectorstores.faiss import FAISS
-
-# Load Data
-# loader = TextLoader("./state_of_the_union.txt")
-# documents = loader.load()
 
 loader = PyPDFLoader("data/contract.pdf")
 pages = loader.load_and_split()
 
 from langchain.text_splitter import CharacterTextSplitter
-
 
 text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 chunks = text_splitter.split_documents(pages)
